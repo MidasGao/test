debfd1c4104b5ee27c17d8ca9e6d5089d508b268
Midas2003 <113357283+MidasGao@users.noreply.github.com>
2023-09-01 19:50:55 +0800

    Update file.c

--- file.c
+++ file.c
@@ -1,12 +1,1 @@
-#include <stdio.h>
-
-int main(){
-<<<<<<< HEAD
-	printf("this is a testing program");
-	return 0;
-}
-=======
-  printf("hello world.");
-  return 0;
-}
->>>>>>> a897017ca36ad6152b6f38ef26fc34e4ba811ea0
+111111111111111111
