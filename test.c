836752e7f7e401b7454476d86c96a228fc51ca51
MidasGao <2542413208@qq.com>
2023-09-01 18:54:34 +0800

    首次修改

--- test.c
+++ test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main(){
-	printf("testing...\n");
-	return 0;
+	printf("\n");
+printf("\n");
+printf("\n");
+
+printf("\n");
+
+printf("\n");
+
+printf("\n");
+printf("\n");
+
+
 }
